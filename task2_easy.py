b9ba6513c602ee290cf2c0070072d3a8b95109b3
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-05-01 18:03:07 +0300

    Update task2_easy.py

--- task2_easy.py
+++ task2_easy.py
@@ -21,3 +21,42 @@ def main():
 if __name__ == "__main__":
     main()
 
+#---------------------------------------------------
+
+def caesar_cipher(text, shift):
+
+    # Программа написана под АНГЛИЙСКИЙ ТЕКСТ!!!
+    
+    encrypted_text = ""
+    for char in text:
+        
+        # Проверяем, является ли символ буквой
+        if char.isalpha():
+            
+            # Определяем смещение для каждой буквы
+            shifted = ord(char) + shift
+            
+            # Если выходим за пределы алфавита, возвращаемся к его началу
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+                elif shifted < ord('a'):
+                    shifted += 26
+                    
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+                elif shifted < ord('A'):
+                    shifted += 26
+                    
+            encrypted_text += chr(shifted)
+            
+        else:
+            # Если символ не является буквой, просто добавляем его в зашифрованный текст
+            encrypted_text += char
+            
+    return encrypted_text
+
+def caesar_decipher(text, shift):
+    return caesar_cipher(text, -shift)
+
